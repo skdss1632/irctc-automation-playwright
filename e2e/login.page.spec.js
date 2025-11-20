5a9f175b3e382f2de464498aef3422fc9e93eb64
Ankush Kumar <ankush.kumar@gotobots.com>
2025-11-20 14:38:43 +0530

    wip: login page

---
+++ e2e/login.page.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require("@playwright/test");
+
+test("should validate user login workflow successfully", async ({ page }) => {
+  await page.goto("https://www.irctc.co.in/nget/train-search");
+
+  const alertDialog = page.getByRole("dialog", { name: "Alert" });
+  await alertDialog.getByRole("button", { name: /press ok to confirm/i }).click();
+
+
+
+  // await page.getByRole("button", { name: "OK" }).click();
+
+
+  // precise login button
+  await page.getByRole("link", { name: /click here to login/i }).click();
+
+  // await page.fill("#username", "");
+  // await page.fill("#password", "");
+});
