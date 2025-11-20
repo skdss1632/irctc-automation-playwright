77e5fafc30efaa3b0c29295c6132cd54cb84e82a
Skdss1632 <skdss1632@gmail.com>
2025-11-21 01:51:56 +0530

    wip: login page

--- tests/login.page.spec.js
+++ tests/login.page.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require("@playwright/test");
 test("should validate user login workflow successfully", async ({ page }) => {
   await page.goto("https://www.irctc.co.in/nget/train-search");
 
-  await page.locator(".ui-overflow-hidden").click();
+  await page.locator("page.locator('body.ui-overflow-hidden')").click();
   await page.waitForTimeout(5000);
 
   await page.getByText("Login").click();
